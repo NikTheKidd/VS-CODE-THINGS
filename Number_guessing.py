dfaf9af51783023a0c562bf5c872418e5017491a
Nikilas Blumenstock <74520389+NikTheKidd@users.noreply.github.com>
2026-05-08 14:10:42 -0400

    Refactor Number Guessing Game logic
    Refactor number guessing game to improve structure and readability.

--- Number_guessing.py
+++ Number_guessing.py
@@ -1,191 +1,87 @@
+import random
+import sys
+
+print("Welcome to the Number Guessing Game!")
+print("------------------------------------")
 
 
+def play_game(max_num, attempts, difficulty):
 
-print("Welcome to the number guessing game!")
-print("-------------------------------------")
+    number = random.randint(1, max_num)
+    current_attempt = 1
 
-print("What difficulty do you want?")
-while True: #main menue
-    atmp = 0
-    difi = int(input("""
-    1) Easy (1-10 5 attempts)            
-                    
-    2) Medium (1-50 7 attempts) 
-                            
-    3) Hard (1-100 10 attempts)   
+    print(f"\nYou chose {difficulty} mode.")
+    print(f"You have {attempts} attempts.")
+    print(f"(Debug Number: {number})")  # remove later if you want
 
-    4) Exit     
-                    
-    Enter here: """))
+    while current_attempt <= attempts:
 
-    import random
+        try:
+            guess = int(input(f"\nAttempt {current_attempt}/{attempts}"
+                              f" - Guess a number (1-{max_num}): "))
+
+        except ValueError:
+            print("Bro typed something that isn't even a number.")
+            print("Game over.")
+            sys.exit()
+
+        if guess < 1 or guess > max_num:
+            print("That number isn't even in the range.")
+            print("Congratulations, you played yourself.")
+            sys.exit()
+
+        if guess == number:
+            print("YOU GOT IT!")
+            return
+
+        elif guess < number:
+            print("Too low. Go higher.")
+
+        else:
+            print("Too high. Chill out.")
+
+        current_attempt += 1
+
+    print("\nYOU LOST.")
+    print(f"The number was {number}.")
 
 
-#this is the exist
-    if difi == 4:
-        print("Exiting...")
+while True:
+
+    try:
+        choice = int(input("""
+Choose a difficulty:
+
+1) Easy   (1-10, 5 attempts)
+2) Medium (1-50, 7 attempts)
+3) Hard   (1-100, 10 attempts)
+4) Exit
+
+Enter here: """))
+
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
+
+    if choice == 1:
+        play_game(10, 5, "Easy")
+
+    elif choice == 2:
+        play_game(50, 7, "Medium")
+
+    elif choice == 3:
+        play_game(100, 10, "Hard")
+
+    elif choice == 4:
+        print("Exiting game...")
         break
 
+    else:
+        print("Invalid option.")
+        continue
 
+    again = input("\nPlay again? (yes/no): ").strip().lower()
 
-
-#this is easy mode
-    if difi == 1:
-        print("You chose easy mood PUSSY ")
-        import random
-        num = random.randint(1, 10)
-        atmp = 1
-        print(num) 
-        while True:
-            try:
-                user = int(input("Take your guess (1-10): "))
-                print(f'{atmp})')
-            except ValueError:
-                print("really nigga your done")
-                print("Exiting")
-                exit()
-
-        
-            #if they go more than they are supposed to
-            if user > 10:
-                print("really nigga your done")
-                print("Exiting...")
-                exit()
-
-            if user == num:
-                print("You got it!!!")
-                #lay again
-                ans = input("Would you like to play again? yes or no : ").strip().lower()
-                if ans == "yes":
-                    break
-                else:
-                    print("Exiting...")
-                    exit()
-
-            elif atmp > 4:
-                print("YOU LOOSE!!!")
-                #play again
-                ans = input("Would you like to play again? yes or no : ").strip().lower()
-                if ans == "yes":
-                    break
-                else:
-                    print("Exiting...")
-                    exit()
-                
-            
-            elif user < num:
-                print("Higher try again")
-                atmp += 1
-                
-            
-            elif user > num:
-                print("Too high try again")
-                atmp += 1
-            
-       
-       
-       
-       #medium mode 
-    elif difi == 2:
-        print("You chose Medium mode ")
-        import random
-        num = random.randint(1, 50)
-        atmp = 1
-        print(num) 
-        while True:
-            try:
-                user = int(input("Take your guess (1-50): "))
-                print(f'{atmp})')
-            except ValueError:
-                print("Really nigga bye.")
-                print("Exiting")
-                exit()
-
-            #if they go more than they are supposed to
-            if user > 50:
-                print("really nigga your done")
-                print("Exiting...")
-                exit()
-
-            if user == num:
-                print("You got it!!!")
-               #play agin
-                ans = input("Would you like to play again?yes or no : ").strip().lower()
-                if ans == "yes":
-                    break
-                else:
-                    print("Exiting...")
-                    exit()
-                
-
-            elif atmp > 7:
-                print("YOU LOOSE!!!")
-                # play again
-                ans = input("Would you like to play again? yes or no : ").strip().lower()
-                if ans == "yes":
-                    break
-                else:
-                    print("Exiting...")
-                    exit()
-            
-            elif user < num:
-                print("Higher try again")
-                atmp += 1
-                
-            
-            elif user > num:
-                print("Too high try again")
-                atmp += 1
-
-
-
-#hard mode
-    if difi == 3:
-        print("You chose Hard mode ")
-        import random
-        num = random.randint(1, 100)
-        atmp = 1
-        print(num) 
-        while True:
-            try:
-                user = int(input("Take your guess (1-100): "))
-                print(f'{atmp})')
-            except ValueError:
-                print("Really nigga bye.")
-                print("Exiting")
-                exit()
-
-            #if they go more than they are supposed to
-            if user > 100:
-                print("really nigga your done")
-                print("Exiting...")
-                exit()
-
-            if user == num:
-                print("You got it!!!")
-                #play again
-                ans = input("Would you like to play again? yes or no : ").strip().lower()
-                if ans == "yes":
-                    break
-                else:
-                    print("Exiting...")
-                    exit()
-
-            elif atmp > 10:
-                print("YOU LOOSE!!!")
-                #play again
-                ans = input("Would you like to play again? yes or no: ").strip().lower()
-                if ans == "yes":
-                    break
-                else:
-                    print("Exiting...")
-                    exit()
-            
-            elif user < num:
-                print("Higher try again")
-                atmp += 1
-                
-            
-            elif user > num:
-                print("Too high try again")
-                atmp += 1
+    if again != "yes":
+        print("Thanks for playing.")
+        break
